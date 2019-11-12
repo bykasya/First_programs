e3ac140cc56adf4ac563bc4d441abbc87717115c
Asya Bykova <asya.bykova@gmail.com>
2019-11-12 03:53:19 +0000

    restructure

--- hangman.rb
+++ hangman.rb
@@ -2,18 +2,14 @@
 def hangman(word,letter)
   x=word.downcase.chars
   y=[]
-  loop do
    x.each do|i|
-     if i==letter
-     y << letter
+     if letter.include? i
+         y << i
      else y.push "_"
      end
    end     
-  end 
- return y.join
+   return y.join
 end  
 
-puts "Enter a letter"
-l=gets.chomp
-w= hangman("bear","#{l}")
-puts "your guess is #{w}"
+puts hangman("assignment", ["a","e"])
+ 
